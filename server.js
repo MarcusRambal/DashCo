719a61a5a8266ce50202608b18b91f0bd04f5c78
Marcus Rambal Preston <128008102+MarcusRambal@users.noreply.github.com>
2025-04-04 11:56:39 -0500

    Update server.js
    funciona para un solo pais, revisar para varios

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors())
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'CampanaPlateada1902',
   database: 'covid_data'
 })
 
@@ -30,7 +30,50 @@ app.get('/countries', (req, res) => {
       return res.status(500).send('Error en la base de datos')
     }
     res.json(results.map(row => row.entity))
-    console.log('✅ Consulta de países exitosa' + results.map(row => row.entity))
+    console.log('✅ Consulta de países exitosa')
+  })
+})
+
+app.get('/data', (req, res) => {
+  const selectedCountries = req.query.countries.split(',')
+
+  const placeholders = selectedCountries.map(() => '?').join(',')
+
+  const query = `select cc.entity, DATE_FORMAT(day, '%Y-%m-01') as month, SUM(cc.daily_deaths) as total_deaths
+          from casos_covid cc
+          WHERE 
+            entity IN (${placeholders})
+          GROUP BY entity, month
+          ORDER BY entity, month`
+
+  db.query(query, [selectedCountries], (err, results) => {
+    if (err) {
+      console.error('🚨 Error en la consulta:', err)
+      return res.status(500).send('Error en la base de datos')
+    }
+    const grouped = {}
+
+    results.forEach(row => {
+      // eslint-disable-next-line camelcase
+      const { entity, month, total_deaths } = row
+
+      if (!grouped[entity]) {
+        grouped[entity] = []
+      }
+
+      grouped[entity].push({
+        day: month, 
+        daily_deaths: Number(total_deaths)
+      })
+    })
+
+    const formatted = Object.entries(grouped).map(([country, data]) => ({
+      country,
+      data
+    }))
+
+    res.json(formatted)
+    console.log('✅ Consulta de datos exitosa')
   })
 })
 
